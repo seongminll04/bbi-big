358ee507a72fed09aa55c641f22bcb5e40a69c28
seongmin_l <lrune2el@gmail.com>
2024-03-06 17:49:25 +0900

    feat : font, logo 파일 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import logo from './assets/images/logo.png'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <div style={{display:'flex', justifyContent:'center', alignItems:'center'}}>
+          <img src={logo} className="App-logo" alt="logo" />
+          <p className='logofont'>bbi-big</p>
+        </div>
+
+
+
       </header>
     </div>
   );
