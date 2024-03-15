f6dbb5dee21da36db5da40ef814a3e426c9e26e5
youjuny <youyoung2774@gmail.com>
2024-03-15 17:32:52 +0900

    feat: 서버 이름입력추가

--- frontEnd/src/pages/components/modalopen/createserver.tsx
+++ frontEnd/src/pages/components/modalopen/createserver.tsx
@@ -1,24 +1,67 @@
 import React, { useState } from 'react';
 import styles from './createserver.module.css';
+import axios from 'axios';
+import { useDispatch } from 'react-redux';
+import {setModal} from '../../../store/actions';
 
 function CreateServer() {
+  const dispatch = useDispatch();
   const [serverName, setServerName] = useState('');
+  const [serverImg, setServerImg] = useState<FileList | null>(null);
+  const [joinMethod, setJoinMethod] = useState('');
+  const [searchOpen, setSearchOpen] = useState('');
+
   const [modalOpen, setModalOpen] = useState(true);
-  const modalClose = () => {
-    setModalOpen(!modalOpen);
+
+  const create = () => {
+    const formdata = new FormData();
+    if (serverImg) {
+      formdata.append('serverImg', serverImg[0]);
+    }
+
+    formdata.append('serverData', JSON.stringify({
+      serverName:serverName,
+      joinMethod:joinMethod,
+      searchOpen:searchOpen
+    }))
+
+    axios({
+      method:'post',
+      url:process.env.REACT_APP_BACKEND_URL + '/server/create',
+      data : formdata,
+      withCredentials: true,
+      headers : {
+        "Content-Type": "multipart/form-data",
+      }
+    }).then(()=> {
+      dispatch(setModal(null));
+    }).catch(err=> {
+      alert('서버를 생성할 수 없습니다.')
+    })
+  }
+
+  const handleServerName = (value:string) => {
+    setServerName(value);
+  }
+
+  const handleServerImg = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files && event.target.files.length > 0 ) {
+      setServerImg(event.target.files);
+    }
   };
 
-  const handleCreateServer = () => {
-    console.log('서버를 생성합니다:', serverName);
-
+  const modalClose = () => {
+    setModalOpen(!modalOpen);
   };
 
   return (
     <div className={styles.container}>
       <p>서버 만들기</p>
-      <img className={styles.serverImg} src="" alt="" />
-      <button onClick={handleCreateServer}>서버 생성</button>
-      <button onClick={modalClose}>취소</button>
+      <input type='file' className={styles.serverImg} onChange={handleServerImg} />
+      <input type="text" placeholder='서버이름을 입력해주세요' value={serverName} onChange={(event)=>{handleServerName(event.target.value)}} 
+      className={styles.servernameInput} />
+      <button className={styles.button} onClick={create}>서버 생성</button>
+      <button className={styles.button} onClick={modalClose}>취소</button>
     </div>
   );
 }
